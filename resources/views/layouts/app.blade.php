bc9b0c193729aed3d1868d56573bb148d76128fd
dyyyyy23 <158675961+dyyyyy23@users.noreply.github.com>
2025-04-20 00:59:03 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Albert Einstein</title>
+ <title>@yield('title', 'Albert Einstein-Ilmuwan Paling Berpengaruh Sepanjang Masa')</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
